86dd0228808c662af2ee6ef8cc1006920fd03e62
dkulemin <blablamaildk@gmail.com>
2020-09-04 05:46:24 +0000

    adding files *.cpp

---
+++ white/week_1/minimal_string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    if (s1 < s2) {
+        if (s1 < s3) {
+            cout << s1;
+        } else 
+        {
+            cout << s3;
+        }
+    } else
+    {
+        if (s2 < s3) {
+            cout << s2;
+        } else
+        {
+            cout << s3;
+        }
+        
+    }
+    
+    return 0;
+}
